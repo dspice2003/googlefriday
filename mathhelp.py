cee269beb299b6cc112a284aa0b83a70be91cf81
dspice2003 <dspice@sjnma.org>
2017-10-16 07:03:19 -0500

    progress in google friday

---
+++ mathhelp.py
@@ -0,0 +1,49 @@
+grade = input("Hello please enter your grade 2nd to 4th\n:")
+
+
+if(grade == "4th"):
+	print("you have selected 4th grade")
+	print("lets try some practice problems")
+print("x + 25 = 50 what is x?")
+
+x = 25
+x = input("please enter the anwser\n:")
+
+
+if(x == "25"):
+	print("You got It!")
+else:
+	print("Try again")
+
+print("lets try some more")
+
+print("if x = 500 what is x + 500")
+x = 1000
+
+x = input("please enter the anwser\n:")
+
+if(x == "1000"):
+	print("You got It!")
+else:
+	print("Try again")
+
+print("if x = 5000 what is 125 + x")
+
+x = 5125
+x = input("please enter the anwser\n:")
+
+if(x == "5125"):
+	print("You got It!")
+else:
+	print("Try again")
+
+print("Ok you proved yourself now its going to get harder")
+print("what is x + 1500 if x = 20000")
+
+x = 20150
+x = input("please enter the anwser\n:")
+
+if(x == "20150"):
+	print("You Got It!")
+else:
+	print("Try again")
