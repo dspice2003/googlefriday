9bc4080e8b2f170c5ba9b8f96421e2a2e3258390
dspice2003 <dspice@sjnma.org>
2017-10-30 14:12:57 -0500

    google friday progress

--- mathhelp.py
+++ mathhelp.py
@@ -1,25 +1,54 @@
+import time
+
+print("Hello Welcome to MathHelp.com")
+time.sleep(2)
+
+# password code
+
+password = ""
+limit = 0
+
+username = input("Please Enter the programs username\n:")
+password = input("Please Enter the programs password\n:")
+
+
+while(password != "mathelp" and username != "mathelp"):
+	limit = limit + 1
+if(password == "mathelp" and username == "mathelp"):
+	print("YOU ARE LOGED IN")
+else:
+	print("Access Denied")
+time.sleep(2)
+# password code End
+
+# grade code
 
 grade = input("Hello please enter your grade 2nd to 4th\n:")
 
-
+time.sleep(2)
 if(grade == "4th"):
 	print("you have selected 4th grade")
+	time.sleep(2)
 	print("lets try some practice problems")
-
+time.sleep(2)
 if(grade == "3rd"):
 	print("you have selected 3rd grade")
+	time.sleep(2)
 	print("lets try some practice problems")
-
+time.sleep(2)
 if(grade == "2nd"):
 	print("you have selected 2nd grade")
+	time.sleep(2)
 	print("lets try some practice problems")
 
+# grade code End
+
+# math problems code
 
 print("x + 25 = 50 what is x?")
 
 x = 25
 x = input("please enter the anwser\n:")
-
 
 if(x == "25"):
 	print("You got It!")
@@ -58,3 +87,5 @@ if(x == "20150"):
 	print("You Got It!")
 else:
 	print("Try again")
+
+# math problems code End
